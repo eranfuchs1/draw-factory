2d76bb1669358f2e907c2dff67fb7bc753443245
xer <xer@xer-labtop>
2021-09-16 13:57:00 +0300

    removing old comments

--- draw/views.py
+++ draw/views.py
@@ -14,10 +14,6 @@ from PIL import Image
 @api_view(['POST'])
 @parser_classes([JSONParser])
 def test_api_view(request, drawing_tool):
-    """
-    A view that can accept POST requests with JSON content.
-    """
-    # print(request.data)
     tool_model = DrawingTools.objects.get(tool=drawing_tool)
     img_converter = ImageConverter()
     img_converter.put_imgdata(request.data)
@@ -31,7 +27,6 @@ def test_api_view(request, drawing_tool):
     buffer = BytesIO()
     image.save(fp=buffer, format='PNG')
     content_file = ContentFile(buffer.getvalue())
-    #canvas.img = image
     img_name = 'default.png'
     canvas.img = InMemoryUploadedFile(
         content_file,
@@ -55,24 +50,8 @@ def test_api_get_view(request, drawing_tool):
     imgdata = img_converter.convert_to_imgdata()
     imgdata = img_converter.convert_list_to_dict(imgdata)
     return Response(imgdata)
-# Create your views here.
 
 
 def test_html_view(request, drawing_tool):
     context = {'drawing_tool': f'tools/{drawing_tool}.html', 'drawing_tool_name': f'{drawing_tool}'}
-    return render(request, 'test.html', context=context)
-
-
-'''
-def test_api_view(request):
-    if request.method == 'POST':
-        #request.POST
-        #each post, for now just print the
-        #imgdata.
-        for key in request.POST:
-            print(key)
-        #print(request.POST['data'])
-        print(request.DATA)
-
-    return HttpResponse('')
-'''
+    return render(request, 'canvas_styling.html', context=context)
