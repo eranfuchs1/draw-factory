8b78499e358cc22e2fdfea82bd1e15e5f71d5765
xer <xer@xer-labtop>
2021-09-23 20:17:32 +0300

    the path needs to be able to take canvas_id

--- draw/urls.py
+++ draw/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('test_api/<str:drawing_tool>/', test_api_view, name='test_api'),
     path('test_api/<str:drawing_tool>/<int:canvas_id>/', test_api_view, name='test_api_canvas_id'),
     path('test_api_get/<str:drawing_tool>/<str:using_canvas_id>/<str:using_in_use>/', test_api_get_view, name='test_api_get'),
+    path('test_api_get/<str:drawing_tool>/<str:using_canvas_id>/<str:using_in_use>/<int:canvas_id>/', test_api_get_view, name='test_api_get_by_id'),
     path('test_api_get_ids/<str:drawing_tool>', test_api_get_ids_view, name='test_api_get_ids'),
     path('test_api_get_ids/', test_api_get_ids_view, name='test_api_get_ids_last_tool'),
     path('test_html_show_all/', test_show_all_view, name='test_html_show_all'),
