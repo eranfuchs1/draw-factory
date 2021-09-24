dd24bebc8037d7be085154911076d56f5804d793
xer <xer@xer-labtop>
2021-09-24 16:18:56 +0300

    implement canvas count via url argument

--- draw/views.py
+++ draw/views.py
@@ -109,11 +109,12 @@ def test_html_view(request, drawing_tool):
     return render(request, 'canvas_styling_for_conveyor_belt.html', context=context)
 
 
-def test_show_all_view(request, page_number=None):
+def test_show_all_view(request, page_number=None, canvas_count=None):
     context = {}
     drawing_tools = DrawingTools.objects.all().order_by('order')
     context['drawing_tool'] = drawing_tools.last().tool
     context['page_number'] = page_number
+    context['canvas_count'] = 10 if canvas_count == None else canvas_count
     return render(request, 'show_all/index.html', context=context)
 
 @api_view(['GET'])
