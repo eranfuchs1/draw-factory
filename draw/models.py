db51535e0f051a00a116222d0b0df60c34d40d5d
xer <eranfuchs.website@gmail.com>
2021-10-06 21:16:01 +0300

    adding timestamp for the canvas model in_use_ts

--- draw/models.py
+++ draw/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+ # Create your models here.
 
-# Create your models here.
 class DrawingTools(models.Model):
     order = models.IntegerField()
     tool = models.CharField(max_length=80)
@@ -10,3 +10,4 @@ class Canvas(models.Model):
     img = models.ImageField()
     last_tool = models.ForeignKey(DrawingTools, on_delete=models.CASCADE, null=True)
     in_use = models.BooleanField(null=True)
+    in_use_ts = models.DateTimeField(auto_now=True)
